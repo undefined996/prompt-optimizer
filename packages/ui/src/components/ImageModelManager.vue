639169929a03668211547e4913f53fd770f3661f
linshen <32978552+linshenkx@users.noreply.github.com>
2026-03-12 22:14:33 +0800

    fix(ui): restore model manager action labels

--- packages/ui/src/components/ImageModelManager.vue
+++ packages/ui/src/components/ImageModelManager.vue
@@ -66,27 +66,21 @@
         
         <template #header-extra>
           <NSpace @click.stop :size="4" class="image-model-actions">
-            <NTooltip trigger="hover">
-              <template #trigger>
-                <NButton
-                  @click="testConnection(config.id)"
-                  size="small"
-                  quaternary
-                  circle
-                  :title="t('modelManager.testConnection')"
-                  :disabled="isTestingConnectionFor(config.id)"
-                  :loading="isTestingConnectionFor(config.id)"
-                >
-                  <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
-                      <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
-                      <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
-                    </svg>
-                  </template>
-                </NButton>
+            <NButton
+              @click="testConnection(config.id)"
+              size="small"
+              quaternary
+              :disabled="isTestingConnectionFor(config.id)"
+              :loading="isTestingConnectionFor(config.id)"
+            >
+              <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
+                  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
+                  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
+                </svg>
               </template>
-              {{ t('modelManager.testConnection') }}
-            </NTooltip>
+              <span class="hidden md:inline">{{ t('modelManager.testConnection') }}</span>
+            </NButton>
 
             <!-- 测试结果缩略图 -->
             <NImage
@@ -100,91 +94,67 @@
               :alt="t('image.connection.testImagePreview')"
             />
 
-            <NTooltip trigger="hover">
-              <template #trigger>
-                <NButton
-                  @click="editConfig(config.id)"
-                  size="small"
-                  quaternary
-                  circle
-                  :title="t('modelManager.editModel')"
-                >
-                  <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                    </svg>
-                  </template>
-                </NButton>
+            <NButton
+              @click="editConfig(config.id)"
+              size="small"
+              quaternary
+            >
+              <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
               </template>
-              {{ t('modelManager.editModel') }}
-            </NTooltip>
+              <span class="hidden md:inline">{{ t('modelManager.editModel') }}</span>
+            </NButton>
 
-            <NTooltip trigger="hover">
-              <template #trigger>
-                <NButton
-                  @click="cloneConfig(config.id)"
-                  size="small"
-                  quaternary
-                  circle
-                  :title="t('modelManager.cloneModel')"
-                >
-                  <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-                      <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1" />
-                    </svg>
-                  </template>
-                </NButton>
+            <NButton
+              @click="cloneConfig(config.id)"
+              size="small"
+              quaternary
+            >
+              <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                  <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+                </svg>
               </template>
-              {{ t('modelManager.cloneModel') }}
-            </NTooltip>
+              <span class="hidden md:inline">{{ t('modelManager.cloneModel') }}</span>
+            </NButton>
 
-            <NTooltip trigger="hover">
-              <template #trigger>
-                <NButton
-                  @click="toggleConfig(config)"
-                  size="small"
-                  quaternary
-                  circle
-                  class="image-model-toggle"
-                  :title="config.enabled ? t('common.disable') : t('common.enable')"
-                >
-                  <template #icon>
-                    <svg v-if="config.enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
-                      <path d="M12 6v.343"/>
-                      <path d="M18.218 18.218A7 7 0 0 1 5 15V9a7 7 0 0 1 .782-3.218"/>
-                      <path d="M19 13.343V9A7 7 0 0 0 8.56 2.902"/>
-                      <path d="M22 22 2 2"/>
-                    </svg>
-                    <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
-                      <rect x="5" y="2" width="14" height="20" rx="7"/>
-                      <path d="M12 6v4"/>
-                    </svg>
-                  </template>
-                </NButton>
+            <NButton
+              @click="toggleConfig(config)"
+              size="small"
+              :type="config.enabled ? 'warning' : 'success'"
+              quaternary
+            >
+              <template #icon>
+                <svg v-if="config.enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
+                  <path d="M12 6v.343"/>
+                  <path d="M18.218 18.218A7 7 0 0 1 5 15V9a7 7 0 0 1 .782-3.218"/>
+                  <path d="M19 13.343V9A7 7 0 0 0 8.56 2.902"/>
+                  <path d="M22 22 2 2"/>
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
+                  <rect x="5" y="2" width="14" height="20" rx="7"/>
+                  <path d="M12 6v4"/>
+                </svg>
               </template>
-              {{ config.enabled ? t('common.disable') : t('common.enable') }}
-            </NTooltip>
+              <span class="hidden md:inline">{{ config.enabled ? t('common.disable') : t('common.enable') }}</span>
+            </NButton>
 
-            <NTooltip trigger="hover">
-              <template #trigger>
-                <NButton
-                  @click="deleteConfig(config.id)"
-                  size="small"
-                  type="error"
-                  quaternary
-                  circle
-                  :title="t('common.delete')"
-                >
-                  <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.11 0 0 0-7.5 0" />
-                    </svg>
-                  </template>
-                </NButton>
+            <NButton
+              @click="deleteConfig(config.id)"
+              size="small"
+              type="error"
+              quaternary
+            >
+              <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.11 0 0 0-7.5 0" />
+                </svg>
               </template>
-              {{ t('common.delete') }}
-            </NTooltip>
+              <span class="hidden md:inline">{{ t('common.delete') }}</span>
+            </NButton>
           </NSpace>
         </template>
       </NCard>
@@ -197,7 +167,7 @@ import { ref, onMounted, inject, h } from 'vue'
 
 import { useI18n } from 'vue-i18n'
 import {
-  NSpace, NCard, NText, NTag, NButton, NEmpty, NImage, NTooltip, useDialog
+  NSpace, NCard, NText, NTag, NButton, NEmpty, NImage, useDialog
 } from 'naive-ui'
 import { useImageModelManager } from '../composables/model/useImageModelManager'
 import { useToast } from '../composables/ui/useToast'
@@ -446,11 +416,8 @@ defineExpose({
 }
 
 .image-model-actions {
+  align-items: center;
   flex-wrap: nowrap;
-}
-
-.image-model-toggle {
-  color: var(--n-text-color);
 }
 
 /* 文本截断样式 */
